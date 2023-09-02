130917e958081cf573cd4459ed5a7bf6a183fe2b
Aitor S.C <aitorsancab@gmail.com>
2023-09-02 11:27:54 +0100

    Disparo en todas las direcciones

--- bullet.js
+++ bullet.js
@@ -11,7 +11,7 @@ function Bullet(player) {
         this.x = player.x
         switch (this.direction) {
             case 'up':
-               this.y= player.y-1
+               this.y = player.y - 1
                var bulletCell = document.querySelector(`#row${this.y} #col${this.x}`)
                for(this.y ; !document.querySelector(`#row${this.y} #col${this.x}`).classList.contains('wall'); this.y--){
                     document.querySelector(`#row${this.y} #col${this.x}`).classList.add("bullet")
@@ -19,22 +19,30 @@ function Bullet(player) {
                }
                 break
             case 'left':
-                var bulletCell = document.querySelector(`#row${player.y} #col${player.x - 1}`)
-                bulletCell.classList.add("bullet")
-                this.y = player.y
-                this.x = player.x -1
+                this.x = player.x - 1
+                var bulletCell = document.querySelector(`#row${this.y} #col${this.x}`)
+                for(this.y ; !document.querySelector(`#row${this.y} #col${this.x}`).classList.contains('wall'); this.x--){
+                    document.querySelector(`#row${this.y} #col${this.x}`).classList.add("bullet")
+                    this.cells.push(document.querySelector(`#row${this.y} #col${this.x}`))
+                }
                 break
+
             case 'down':
-                var bulletCell = document.querySelector(`#row${player.y + 1} #col${player.x}`)
-                bulletCell.classList.add("bullet")
                 this.y = player.y + 1
-                this.x = player.x
+                var bulletCell = document.querySelector(`#row${this.y} #col${this.x}`)
+                for(this.y ; !document.querySelector(`#row${this.y} #col${this.x}`).classList.contains('wall'); this.y++){
+                    document.querySelector(`#row${this.y} #col${this.x}`).classList.add("bullet")
+                    this.cells.push(document.querySelector(`#row${this.y} #col${this.x}`))
+                }
                 break
+
             case 'right':
-                var bulletCell = document.querySelector(`#row${player.y} #col${player.x + 1}`)
-                bulletCell.classList.add("bullet")
-                this.y = player.y 
                 this.x = player.x + 1
+                var bulletCell = document.querySelector(`#row${this.y} #col${this.x}`)
+                for(this.y ; !document.querySelector(`#row${this.y} #col${this.x}`).classList.contains('wall'); this.x++){
+                    document.querySelector(`#row${this.y} #col${this.x}`).classList.add("bullet")
+                    this.cells.push(document.querySelector(`#row${this.y} #col${this.x}`))
+                }
                 break
         }
         setTimeout(this.erase,200)
@@ -45,6 +53,8 @@ function Bullet(player) {
     this.erase = function () {
         self.cells.forEach(element => {element.classList.remove("bullet")})
         this.cells=[]
+        
+
     }
     
 }
