97a01708abeda09ec003c500e86f0efd8c385430
Fersoher <74140565+Fersoher@users.noreply.github.com>
2023-09-06 10:37:56 +0100

    Musicote V1.0

--- index.js
+++ index.js
@@ -8,31 +8,32 @@ var enemy1 = new Enemy(10, 3)
 var enemy2 = new Enemy(4, 6)
 var enemy3 = new Enemy(14, 5)
 const audio = new Audio("assets/Density & Time - MAZE.mp3");
-audio.play();
-     function createBoard() {
-       
-        var obstacles = [];
-        var table = document.getElementById("board");
-        boundMap.forEach((row, i) => {
-            const tr = document.createElement("tr");
-            tr.setAttribute("id", "row" + i);
-            row.forEach(function (col, j) {
-                const td = document.createElement("td");
-                td.setAttribute("id", "col" + j);
-                if (col === "-") {
-                    td.classList.add("wall");
-                    obstacles.push({ x: j, y: i });
-                }
-                tr.appendChild(td);
-            });
-            table.appendChild(tr);
-        });
+audio.volume = 0.2;
+audio.play()
+function createBoard() {
 
-        player1.drawPlayer();
-        enemy1.drawEnemy()
-        enemy2.drawEnemy()
-        enemy3.drawEnemy()
-    }
+    var obstacles = [];
+    var table = document.getElementById("board");
+    boundMap.forEach((row, i) => {
+        const tr = document.createElement("tr");
+        tr.setAttribute("id", "row" + i);
+        row.forEach(function (col, j) {
+            const td = document.createElement("td");
+            td.setAttribute("id", "col" + j);
+            if (col === "-") {
+                td.classList.add("wall");
+                obstacles.push({ x: j, y: i });
+            }
+            tr.appendChild(td);
+        });
+        table.appendChild(tr);
+    });
+
+    player1.drawPlayer();
+    enemy1.drawEnemy()
+    enemy2.drawEnemy()
+    enemy3.drawEnemy()
+}
 
 
 
@@ -82,75 +83,76 @@ window.addEventListener('keydown', function (e) {
     if (e.key == 'd') {
         player1.direction = "right"
         player1.movePlayer()
-    
+
     }
     if (e.key == ' ') {
 
-        if (player1.alive){
+        if (player1.alive) {
             var bullet = new Bullet(player1)
             bullet.createShoot()
         }
-        
 
-        
 
-        
+
+
+
     }
-    
+
 })
 
-function gameLoop(){
-    
+function gameLoop() {
+
     if (enemy1.alive) {
         enemy1.moveEnemy()
-        var enemyBullet1 = new Enemybullet(player1,enemy1)    
+        var enemyBullet1 = new Enemybullet(player1, enemy1)
         enemyBullet1.createShoot()
-        
+
     }
     if (enemy2.alive) {
         enemy2.moveEnemy()
-        var enemyBullet2 = new Enemybullet(player1,enemy2)
+        var enemyBullet2 = new Enemybullet(player1, enemy2)
         enemyBullet2.createShoot()
-        
+
     }
     if (enemy3.alive) {
         enemy3.moveEnemy()
-        var enemyBullet3 = new Enemybullet(player1,enemy3)
+        var enemyBullet3 = new Enemybullet(player1, enemy3)
         enemyBullet3.createShoot()
-        
-    }   
+
+    }
     function winScreen() {
         if (enemy1.alive === false && enemy2.alive === false && enemy3.alive === false) {
-             document.querySelector(".wins").style.display = "block";
-             document.querySelector("#board").style.display = "none";
-         }
-         function winagain() {
-             let tryButton = document.querySelector(".winagain");
-             tryButton.onclick = function () {
-                 location.reload();
-             };
-         }
- 
-         winagain();
- 
-     }
- 
- 
-     winScreen()
- }
- 
- 
- function gameOverbutton() {
- 
-         document.querySelector("#start button")
-     let startButton = document.querySelector("#start button");
-     startButton.onclick = function () {
-         createBoard()
-         document.querySelector("#start").style.display = "none";
-         document.querySelector("#scenary").style.display = 'block'}
-     }
- 
- 
- gameOverbutton()
- setInterval(gameLoop, 500)
+            document.querySelector(".wins").style.display = "block";
+            document.querySelector("#board").style.display = "none";
+        }
+        function winagain() {
+            let tryButton = document.querySelector(".winagain");
+            tryButton.onclick = function () {
+                location.reload();
+            };
+        }
+
+        winagain();
+
+    }
+
+
+    winScreen()
+}
+
+
+function gameOverbutton() {
+
+    document.querySelector("#start button")
+    let startButton = document.querySelector("#start button");
+    startButton.onclick = function () {
+        createBoard()
+        document.querySelector("#start").style.display = "none";
+        document.querySelector("#scenary").style.display = 'block'
+    }
+}
+
+
+gameOverbutton()
+setInterval(gameLoop, 500)
 
